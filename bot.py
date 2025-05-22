8320465b596aac46dfbd66ab6b7e483c500ef3b9
GL1KK <danya_serov05@internet.ru>
2025-05-22 14:59:30 +0300

    WebServ

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
 from aiogram.filters import Command
 import asyncio
-import logging
+#import logging
 from dotenv import load_dotenv
 import os
+import uvicorn
+import threading
+from fastapi import FastAPI
 
 load_dotenv()
 
@@ -13,12 +16,25 @@ IMAGES_DIR = os.path.join(BASE_DIR, "images")
 
 TOKEN=os.getenv("TOKEN")
 
-logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TOKEN)
 
 dp = Dispatcher()
 
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Bot is alive!"}
+
+def run_web_server():
+    uvicorn.run(app, host="0.0.0.0", port=8080) 
+
+async def main():
+    web_server_thread = threading.Thread(target=run_web_server)
+    web_server_thread.start()
+    await dp.start_polling(bot)
 @dp.message(Command("start"))
 async def start_handler(message: Message):
     await message.answer("Привет напиши число от 1 до 10 и я отправлю тебе фото")
@@ -34,9 +50,6 @@ async def send_photo(message: Message):
             await message.answer("Дипазон от 1 до 10! Повторите попытку")
     else:
         await message.answer("Введите число!")
-
-async def main():
-    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
